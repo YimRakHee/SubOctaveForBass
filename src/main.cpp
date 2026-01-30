400c39568a7e6db70b6ff682419ece9174bc4a84
YimRakHee <kimseongsik9@gmail.com>
2026-01-30 10:45:30 +0800

    avoid initial xrun

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,23 @@ public:
     float last_sample;
     float last_tone;
     float out_alpha;
+    float in_alpha;
 
     int state;
     int flip_flop;
 
     MyOctavePedal(double rate) :
     in_lpf_state(0.0f), out_lpf_state(0.0f),
-    sample_rate((float)rate), last_sample(0.0f), last_tone(-1.0f),
-    out_alpha(0.0f), state(1), flip_flop(0) {}
+    sample_rate((float)rate), last_sample(0.0f),
+    last_tone(-1.0f), out_alpha(0.0f),
+    state(1), flip_flop(0)
+    {
+        in_alpha = (2.0f * 3.14159265f * 150.0f) / sample_rate;
+    }
 };
 
 static LV2_Handle instantiate(const LV2_Descriptor* descriptor, double rate, const char* bundle_path, const LV2_Feature* const* features) {
+
     return new MyOctavePedal(rate);
 }
 
@@ -54,24 +60,25 @@ static void connect_port(LV2_Handle instance, uint32_t port, void* data) {
 static void run(LV2_Handle instance, uint32_t n_samples) {
     MyOctavePedal* self = (MyOctavePedal*)instance;
 
+    if (!self->input || !self->output || !self->tone_val) return;
+
     float dry_vol = *self->dry_lev;
     float wet_vol = *self->wet_lev;
     float tone_freq = *self->tone_val;
 
-    const float in_alpha = (2.0f * 3.14159265f * 150.0f) / self->sample_rate;
-
-        if (tone_freq != self->last_tone) {
+    if (std::abs(tone_freq - self->last_tone) > 0.0001f) {
         self->out_alpha = (2.0f * 3.14159265f * tone_freq) / self->sample_rate;
         self->last_tone = tone_freq;
     }
 
-    float out_a = self->out_alpha;
+    const float in_a = self->in_alpha;
+    const float out_a = self->out_alpha;
     const float threshold = 0.001f;
 
     for (uint32_t i = 0; i < n_samples; ++i) {
         float in = self->input[i];
 
-        self->in_lpf_state += in_alpha * (in - self->in_lpf_state);
+        self->in_lpf_state += in_a * (in - self->in_lpf_state);
         float analysis_sig = self->in_lpf_state;
 
         if (self->last_sample <= threshold && analysis_sig > threshold) {
@@ -83,16 +90,16 @@ static void run(LV2_Handle instance, uint32_t n_samples) {
         }
         self->last_sample = analysis_sig;
 
-       float oct_raw = (float)self->state * std::abs(in);
-
-       self->out_lpf_state += out_a * (oct_raw - self->out_lpf_state);
-
+        float oct_raw = (float)self->state * std::abs(in);
+        self->out_lpf_state += out_a * (oct_raw - self->out_lpf_state);
 
         if (std::abs(self->out_lpf_state) < 1e-15f) self->out_lpf_state = 0.0f;
 
         self->output[i] = (in * dry_vol) + (self->out_lpf_state * wet_vol);
     }
 }
+
+
 
 static void cleanup(LV2_Handle instance) {
     delete (MyOctavePedal*)instance;
